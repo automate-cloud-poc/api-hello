d161f1902779cb98f9c33beea0bf19d2a385a114
Daniel Kanczuk <danielsussa@gmail.com>
2021-07-26 18:59:47 -0300

    new endpoint

--- main.go
+++ main.go
@@ -15,5 +15,10 @@ func main() {
 		fmt.Println("log backup")
 		return c.String(http.StatusOK, "Hello, World! version 0.0.4")
 	})
+	e.GET("/hello/v1/sayHello", func(c echo.Context) error {
+		log.Println("my hello log")
+		fmt.Println("log backup")
+		return c.String(http.StatusOK, "Hello, World from v1! version 0.0.4")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
